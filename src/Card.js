54ceb00e15c3a87e1333c563ede3e0cf2be72345
James Traynor <>
2019-06-23 11:30:58 +0100

    added a few components to get started

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id }) => {
     return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-55'>
+        <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
         <div>
-        <img src='https://robohash.org/test?200x200' alt='robots'/>
-        <div>
-            <h2>Jane Doe</h2>
-            <p>jane.doe@gmail.com</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
         </div>
         </div>
     )
 }
 
-export default Card;
+export default Card; 
